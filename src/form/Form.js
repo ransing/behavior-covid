d732c7c2fdbd25b821d190a296a93f762d308dd2
viraj <virajransing@gmail.com>
2020-03-25 05:01:34 -0400

    still no handle

--- src/form/Form.js
+++ src/form/Form.js
@@ -47,59 +47,33 @@ const App = ({ classes }) => {
       <AppBar position="static" className={classes.appBar}>
         <Toolbar>
           <Typography variant="h6" color="inherit" noWrap>
-            Multi Step Form
+            Behavioral Questions COVID-19
           </Typography>
           <IconButton
             color="inherit"
             aria-label="Github"
             style={{ marginLeft: "auto" }}
           >
-            <Link
-              title="Github"
-              component="a"
-              rel="noopener noreferrer"
-              target="_blank"
-              href="http://github.com/awran5/react-material-ui-step-form"
-              color="inherit"
-            >
-              <SvgIcon component="svg">
-                <path d="M12 .3a12 12 0 0 0-3.8 23.4c.6.1.8-.3.8-.6v-2c-3.3.7-4-1.6-4-1.6-.6-1.4-1.4-1.8-1.4-1.8-1-.7.1-.7.1-.7 1.2 0 1.9 1.2 1.9 1.2 1 1.8 2.8 1.3 3.5 1 0-.8.4-1.3.7-1.6-2.7-.3-5.5-1.3-5.5-6 0-1.2.5-2.3 1.3-3.1-.2-.4-.6-1.6 0-3.2 0 0 1-.3 3.4 1.2a11.5 11.5 0 0 1 6 0c2.3-1.5 3.3-1.2 3.3-1.2.6 1.6.2 2.8 0 3.2.9.8 1.3 1.9 1.3 3.2 0 4.6-2.8 5.6-5.5 5.9.5.4.9 1 .9 2.2v3.3c0 .3.1.7.8.6A12 12 0 0 0 12 .3" />
-              </SvgIcon>
-            </Link>
+            
           </IconButton>
         </Toolbar>
       </AppBar>
       <main className={classes.layout}>
         <Typography variant="h4" align="center" style={{ marginTop: 80 }}>
-          Multi Step Form
+          A moment of reflection
         </Typography>
         <Typography
           variant="subtitle2"
           align="center"
           style={{ marginTop: 10, marginBottom: 10 }}
         >
-          React Material UI multi step form with basic form validation logic.
+          Please answer the questions based of how you have been feeling since last visit. 
         </Typography>
         <Paper className={classes.paper}>
           <StepForm />
         </Paper>
         <Divider style={{ marginTop: 100 }} />
-        <Typography
-          component="p"
-          align="center"
-          style={{ margin: "10px 0", fontSize: ".75rem" }}
-        >
-          Built with{" "}
-          <span role="img" aria-label="Emojis">
-            ❤️
-          </span>{" "}
-          by the{" "}
-          <a href="https://gkstyle.net/" title="GK STYLE">
-            {" "}
-            GK STYLE{" "}
-          </a>
-          team.
-        </Typography>
+        
       </main>
     </div>
   )
